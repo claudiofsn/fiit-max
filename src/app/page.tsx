54fe265bfb161eeea706372dcce47f3c14d6686f
Claudio Neto <craudiofelix@gmail.com>
2025-03-13 07:12:07 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1>FIIT<br></br>MAX</h1>
+        <span>Transforme seu corpo, fortaleça sua mente!</span>
+        <button>CONHECER OS PLANOS</button>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
       </footer>
